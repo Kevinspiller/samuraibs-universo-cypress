eea376bf59aff0c2491b9404d72db341be293361
Kevin Spiller <kevinspillereazy@gmail.com>
2022-05-30 20:46:49 -0300

    Removido timeout mandatório e adicionado timeout implícito

--- cypress/support/components/toast/index.js
+++ cypress/support/components/toast/index.js
@@ -2,9 +2,9 @@ import { el } from './elements' //componente toast
 
 class Toast {
     shouldHaveText(expectText) { //valida texto da saída
-        cy.get(el.toast, {timeout: 15000 })
+        cy.get(el.toast)
             .should('be.visible')
-            .should('have.css', 'opacity', '1', {timeout: 1500})
+            .should('have.css', 'opacity', '1')
             .find('p')
             .should('have.text', expectText)
     }
