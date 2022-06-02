979cbf2c48d4331b076c6b7dd4cf5ee52c077c86
Kevin Spiller <kevinspillereazy@gmail.com>
2022-06-02 17:03:47 -0300

    Terminando módulo tópicos avançados

--- cypress/support/components/toast/index.js
+++ cypress/support/components/toast/index.js
@@ -4,6 +4,7 @@ class Toast {
     shouldHaveText(expectText) { //valida texto da saída
         cy.get(el.toast, {timeout: 15000 })
             .should('be.visible')
+            .should('have.css', 'opacity', '1', {timeout: 1500})
             .find('p')
             .should('have.text', expectText)
     }
